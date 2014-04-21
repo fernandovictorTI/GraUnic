37800aa192bc529a21c88fe79f91f9f3b21a1e9d
Fernando Victor <fernandovictorti@gmail.com>
2014-04-21 04:01:05 -0300

    modificado index

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
 <?php
     header("Location: view/principal.php");
-//	require_once ('business/busGraunic.php');
-//	$dados = ListarDados();
-//	
-//	foreach($dados as $lstDados)
-//	{
-//		echo $lstDados["nome"]."<br />";
-//	}
